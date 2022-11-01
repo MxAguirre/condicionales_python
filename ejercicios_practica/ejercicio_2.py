2da2ff319715e9535c529a67c8aae7c4bf213d07
MxAguirre <115966318+MxAguirre@users.noreply.github.com>
2022-10-31 22:00:40 -0300

    Add files via upload

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -26,9 +26,13 @@ texto_2 = str(input('Ingrese la segunda palabra:\n'))
 # - Si texto_1 es mayor (alfabéticamente) a texto_2, 
 #   almacenar 1 en res_1
 # - De lo contrario, almacenar 2 en res_1
-
+if texto_1 > texto_2:
+    rest_1 = 1
+else:
+    rest_1 = 2
 
 # Imprimir en pantalla la variable res_1
+print(rest_1)
 
 # Compare cual de las dos palabras tiene mayor
 # cantidad de letras
@@ -36,10 +40,15 @@ texto_2 = str(input('Ingrese la segunda palabra:\n'))
 # - Si texto_1 tiene más letras, almacenar 1 en res_2
 # - Si texto_2 tiene más letras, almacenar 2 en res_2
 # - Si tienen la misma cantidad de letras, almacenar 3 en res_2
-
+if len(texto_1) > len(texto_2):
+    rest_2 = 1
+elif len(texto_1) < len(texto_2):
+    rest_2 = 2
+else:
+    rest_2 = 3
 
 # Imprimir en pantalla la variable res_2
-
+print(rest_2)
 
 
 # Verifique si la primera letra de la primera palabra
@@ -47,7 +56,12 @@ texto_2 = str(input('Ingrese la segunda palabra:\n'))
 # - Si la primera letra de texto_1 es mayor,
 #   almacenar 1 en res_3
 # - De lo contrario, almacenar 2 en res_3
+if texto_1[0] > texto_2[0]:
+    rest_3 = 1
+else:
+    rest_3 = 2
 
 
 # Imprimir en pantalla la variable res_3
+print(rest_3)
 
--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -20,6 +20,9 @@ cantidad_numeros_positivos = 0
 # numero_1
 # numero_2
 # numero_3
+numero_1 = int(input("Ingrese el primer numero: "))
+numero_2 = int(input("Ingrese el segundo numero: "))
+numero_3 = int(input("Ingrese el tercer numero: "))
 
 # Deberá realizar un tres condicionales separados,
 # en cada condicional deberá averiguar si cada número
@@ -27,6 +30,26 @@ cantidad_numeros_positivos = 0
 
 # Por cada número mayor a cero (cada condicional que se cumpla)
 # deberá incrementar en 1 (+= 1) la "variable cantidad_numeros_positivos"
+contador = 0
+if numero_1 >= 0:
+    contador +=1
+    if numero_2 >= 0 :
+        contador +=1
+        if numero_3 >= 0 :
+            contador +=1
+    else:
+        if numero_3 >= 0 :
+            contador +=1
+else:
+    if numero_2 >= 0 :
+        contador +=1
+        if numero_3 >= 0 :
+            contador +=1
+    else:
+        if numero_3 >= 0 :
+            contador +=1
+print(contador)
+
 
 
 # Al finalizar, imprimir en pantalla la variable cantidad_numeros_positivos
